d6b7cf83c876eaed74c677a15037068ead7966a1
antrax <antrax2013@hotmail.com>
2026-01-20 10:18:48 +0100

    start

--- SupermarketReceipt.Test/SupermarketXUnitTest.cs
+++ SupermarketReceipt.Test/SupermarketXUnitTest.cs
@@ -1,38 +1,37 @@
 using System.Collections.Generic;
 using Xunit;
 
-namespace SupermarketReceipt.Test
+namespace SupermarketReceipt.Test;
+
+public class SupermarketXUnitTest
 {
-    public class SupermarketXUnitTest
+    [Fact]
+    public void TenPercentDiscount()
     {
-        [Fact]
-        public void TenPercentDiscount()
-        {
-            // ARRANGE
-            SupermarketCatalog catalog = new FakeCatalog();
-            var toothbrush = new Product("toothbrush", ProductUnit.Each);
-            catalog.AddProduct(toothbrush, 0.99);
-            var apples = new Product("apples", ProductUnit.Kilo);
-            catalog.AddProduct(apples, 1.99);
+        // ARRANGE
+        SupermarketCatalog catalog = new Catalog();
+        var toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.AddProduct(toothbrush, 0.99);
+        var apples = new Product("apples", ProductUnit.Kilo);
+        catalog.AddProduct(apples, 1.99);
 
-            var cart = new ShoppingCart();
-            cart.AddItemQuantity(apples, 2.5);
+        var cart = new ShoppingCart();
+        cart.AddItemQuantity(apples, 2.5);
 
-            var teller = new Teller(catalog);
-            teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+        var teller = new Teller(catalog);
+        teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
 
-            // ACT
-            var receipt = teller.ChecksOutArticlesFrom(cart);
+        // ACT
+        var receipt = teller.ChecksOutArticlesFrom(cart);
 
-            // ASSERT
-            Assert.Equal(4.975, receipt.GetTotalPrice());
-            Assert.Equal(new List<Discount>(), receipt.GetDiscounts());
-            Assert.Single(receipt.GetItems());
-            var receiptItem = receipt.GetItems()[0];
-            Assert.Equal(apples, receiptItem.Product);
-            Assert.Equal(1.99, receiptItem.Price);
-            Assert.Equal(2.5 * 1.99, receiptItem.TotalPrice);
-            Assert.Equal(2.5, receiptItem.Quantity);
-        }
+        // ASSERT
+        Assert.Equal(4.975, receipt.GetTotalPrice());
+        Assert.Equal(new List<Discount>(), receipt.GetDiscounts());
+        Assert.Single(receipt.GetItems());
+        var receiptItem = receipt.GetItems()[0];
+        Assert.Equal(apples, receiptItem.Product);
+        Assert.Equal(1.99, receiptItem.Price);
+        Assert.Equal(2.5 * 1.99, receiptItem.TotalPrice);
+        Assert.Equal(2.5, receiptItem.Quantity);
     }
 }
